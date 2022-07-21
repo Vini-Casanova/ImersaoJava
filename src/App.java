49cb673291591b6b0dcd2004352696f6f48a171b
Vinicius Casanova <vininewhouse@gmail.com>
2022-07-21 15:24:22 -0300

    Primeiro Commit

---
+++ src/App.java
@@ -0,0 +1,45 @@
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Conexão HTTP para a api de top 250 filmes
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        
+
+        // titulo, poster, rating
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        //exibir dados 
+        for (Map<String,String> filme : listaDeFilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            var geradora = new StickerGenerator();
+            geradora.criar(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
+            System.out.println( );
+            System.out.println( );
+
+        } 
+    }
+}
