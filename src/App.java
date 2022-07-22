0f88302fcf0acfcb8e6913b731e9f4ce26ea5f58
Vinicius Casanova <vininewhouse@gmail.com>
2022-07-22 00:10:59 -0300

    Refatorando Laços e gitkeep na pasta saida

--- src/App.java
+++ src/App.java
@@ -10,13 +10,18 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
         // Conexão HTTP para a api de top 250 filmes
+
+
         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
+
+        var geradora = new StickerGenerator();
         
 
         // titulo, poster, rating
@@ -24,15 +29,14 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         
         //exibir dados 
-        for (Map<String,String> filme : listaDeFilmes) {
+        for (int i = 0; i < 10; i++) {
+            Map<String,String> filme = listaDeFilmes.get(i);
 
             String urlImagem = filme.get("image");
             String titulo = filme.get("title");
-
-            
             String nomeArquivo = titulo.replace(":", "-") + ".png";
 
-            var geradora = new StickerGenerator();
+            //Bloco de caso não encontre ou de um erro ele envie a mensagem de erro 
             try{
                 InputStream inputStream = new URL(urlImagem).openStream();
                 System.out.println("Gerando imagem - [" + titulo + "]");
@@ -44,8 +48,7 @@ public class App {
 
             System.out.println(titulo);
             System.out.println();
-            System.out.println( );
-            System.out.println( );
+            
 
         } 
         
